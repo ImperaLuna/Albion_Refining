27618d4e21427c6c19cbe1b4ec5343007a0d6e22
ImperaLuna <rares.brezeanu.dev@gmail.com>
2023-09-27 13:25:34 +0300

    show_best_price function

--- wood_refining.py
+++ wood_refining.py
@@ -3,6 +3,7 @@ import os
 from multi_modules_functions import refining_mats_variables
 from multi_modules_functions import api_url_csv
 from multi_modules_functions import save_data_to_csv
+from multi_modules_functions import show_best_price
 
 wood_items = refining_mats_variables('WOOD', 'PLANKS')
 wood_url = api_url_csv(wood_items)
@@ -11,3 +12,7 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 csv_filename = os.path.join(script_directory, 'wood_refining.csv')
 
 save_data_to_csv(wood_url, csv_filename)
+
+ITEM_ID = 'T4_PLANKS'
+show_price = show_best_price(ITEM_ID, csv_filename)
+print(show_price)
