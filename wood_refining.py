4062fcb5aefd61d9ea6cf1df9253c26105f6078e
ImperaLuna <rares.brezeanu.dev@gmail.com>
2023-09-27 04:13:13 +0300

    rearranging variables , delete print test

--- wood_refining.py
+++ wood_refining.py
@@ -4,13 +4,10 @@ from multi_modules_functions import refining_mats_variables
 from multi_modules_functions import api_url_csv
 from multi_modules_functions import save_data_to_csv
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-
-
 wood_items = refining_mats_variables('WOOD', 'PLANKS')
 wood_url = api_url_csv(wood_items)
 
+script_directory = os.path.dirname(os.path.abspath(__file__))
 csv_filename = os.path.join(script_directory, 'wood_refining.csv')
 
-print (wood_url)
 save_data_to_csv(wood_url, csv_filename)
