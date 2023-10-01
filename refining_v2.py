7c98b59a57ab1bead2edd1fcb85549e47eb6e773
ImperaLuna <rares.brezeanu.dev@gmail.com>
2023-10-01 20:21:33 +0300

    function fix

--- refining_v2.py
+++ refining_v2.py
@@ -10,7 +10,7 @@ from multi_modules_functions import show_max_price
 from multi_modules_functions import max_price_refined
 from multi_modules_functions import calculate_daily_profit
 
-variable_name = generate_variable_name('t7.1', 'fiber')
+variable_name = generate_variable_name('t7.1', 'hide')
 
 print()
 print(f'The list generated for your items is: {variable_name}')
@@ -30,7 +30,7 @@ print(f'Total price for 1 craft: {formatted_mats_price}')
 daily_crafts = int(nr_of_daily_crafts(variable_name))
 print(f'Daily number of crafts is: {daily_crafts}')
 
-total_amount = mats_price * daily_crafts
+total_amount = int(mats_price * daily_crafts)
 formatted_total_amount = format(total_amount, ',')
 print(f'Total amount that needs to be invested into resources is: {formatted_total_amount}')
 print()
@@ -39,7 +39,7 @@ show_max_price_df = show_max_price(variable_name, resource_type)
 print(show_max_price_df)
 print()
 
-refined_resource_cost = int(max_price_refined(show_max_price_df))
+refined_resource_cost = int(max_price_refined(show_max_price_df) * 0.935) #tax
 formatted_refined_resource_cost = format(refined_resource_cost, ',')
 print(f'Refined resource cost: {formatted_refined_resource_cost}')
 
@@ -50,7 +50,7 @@ print(f'Value of returned resources is: {formatted_returned_resources}')
 formatted_max_crafted_resources = format(refined_resource_cost * daily_crafts, ',')
 print(f'Max value of crafted resources is: {formatted_max_crafted_resources}')
 
-total_daily_profit = returned_resources + (refined_resource_cost * daily_crafts)
+total_daily_profit = int(returned_resources + (refined_resource_cost * daily_crafts))
 total_daily_profit -= total_amount
 formatted_total_daily_profit = format(total_daily_profit, ',')
 print(f'Total daily profit is: {formatted_total_daily_profit}')
