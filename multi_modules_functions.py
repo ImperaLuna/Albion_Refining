121c4700b935a596930b151bf6a30c82145c6199
ImperaLuna <rares.brezeanu.dev@gmail.com>
2023-10-02 17:58:34 +0300

    rewrote main.py (ex refining_v2, clarify functions

--- multi_modules_functions.py
+++ multi_modules_functions.py
@@ -1,39 +1,67 @@
 '''Store functions used in multiple modules'''
+
+from typing import List
 import pandas as pd
-import refining_calculator as rc
-
-#! ====================================== Building CSV files ======================================
 
 
-#! ====================================== Building CSV files ======================================
-
-def show_best_price(item_ids, csv_filename):
+def show_buy_price_df(item_ids: List[str], csv_filename: str) -> pd.DataFrame:
     '''
     Reads a CSV file, selects specific columns, and returns a DataFrame 
     grouped by 'item_id' and sorted by 'sell_price_max' in ascending order within each group.
 
     Args:
-        item_ids |list| -- List of item IDs to filter the data.
-
-        csv_filename |str| -- File created using fct save_data_to_csv()
+        - item_ids |list of strings| -- Item IDs for filtering data from generate_variable_name()
+        - csv_filename |str| -- File created using fct create_csv_file()
 
     Returns:
-        result_data_frame |DataFrame| -- Grouped and ascended by 'item_id' and 'sell_price_max
+        - buy_materials_df |DataFrame| -- Grouped and ascended by 'item_id' and 'sell_price_max'
     '''
+
+    columns_to_select = ['item_id', 'city', 'sell_price_max', 'sell_price_max_date']
+
+    data = pd.read_csv(csv_filename)
+    data['sell_price_max'] = data['sell_price_max'].astype(float)
+    filtered_data = data[data['item_id'].isin(item_ids)]
+
+    # Group by 'item_id' and sort within each group
+    buy_materials_df = (
+        filtered_data[columns_to_select].groupby('item_id', as_index=True)
+        .apply(lambda x: x.sort_values(by='sell_price_max', ascending=True))
+    )
+
+    return buy_materials_df
+
+def show_sell_price_df(item_ids: List[str], csv_filename: str) -> pd.DataFrame:
+    '''
+    Extract the item_id and edit it to matchT for final refining process product.
+
+    Reads a CSV file, selects specific columns, and returns a DataFrame, 
+    sorted by 'sell_price_max' in descending.
+
+    Args:
+        - item_ids |list of strings|| -- List's [1] holds ID from generate_variable_name()
+        - csv_filename |str| -- File created using fct create_csv_file()
+
+    Returns:
+        sell_refined_resource_df |DataFrame| -- Descended 'sell_price_max
+    '''
+
+    required_item_id = item_ids[1]
+    next_tier = f"{required_item_id[0]}{int(required_item_id[1]) + 1}{required_item_id[2:]}"
 
     selected_columns = ['item_id', 'city', 'sell_price_max', 'sell_price_max_date']
 
     data_frame = pd.read_csv(csv_filename)
     data_frame['sell_price_max'] = data_frame['sell_price_max'].astype(float)
-    filtered_data_frame = data_frame[data_frame['item_id'].isin(item_ids)]
 
-    # Group by 'item_id' and sort within each group
-    result_data_frame = (filtered_data_frame[selected_columns]
-                         .groupby('item_id', as_index=True)
-                         .apply(lambda x: x.sort_values(by='sell_price_max', ascending=True)))
+    filtered_data_frame = data_frame[data_frame['item_id'] == next_tier]
 
-    return result_data_frame
+    sell_refined_resource_df = (
+        filtered_data_frame[selected_columns]
+        .sort_values(by='sell_price_max', ascending=False)
+    )
 
+    return sell_refined_resource_df
 
 
 def generate_variable_name(tier, raw_resource):
@@ -41,11 +69,11 @@ def generate_variable_name(tier, raw_resource):
     Generates variable names based on the input tier and raw resource.
 
     Args:
-        tier |str|: A string representing the item's tier (e.g., 'T4', 'T5').
-        raw_resource |str|: A string representing the raw resource (e.g., 'ore', 'fiber', 'hide').
+        - tier |str|: A string representing the item's tier (e.g., 't4.0', 'T5.2').
+        - raw_resource |str|: A string representing the raw resource (e.g., 'ore', 'fiber', 'hide').
 
     Returns:
-        variable_name |list or None|: A list with variable name(s) if input is invalid.
+        - variable_name |list or None|: A list with variable name(s) if input is invalid.
 
     Constructs variable based on tier, raw resource, refined resource and enchantment level:
     - For 'T4', appends '_{raw_resource}' or '_{raw_resource}_LEVEL{enchantment}@{enchantment}'.
@@ -99,23 +127,22 @@ def generate_variable_name(tier, raw_resource):
         return variable_name
 
 
-######
-
-def strip_variable_name(variable_names):
+def strip_variable_name(variable_names: List[str]) -> List[str]:
     '''
     Extracts and formats the resource types from a list of variable names.
+    Used in generate_resource_csv_filename()
 
     Args:
-        variable_names |list of str|: A list of variable names ex:['T4_WOOD', 'T5_ORE_']).
+        - variable_names |List[str]| -- A list of variable names from generate_variable_name().
 
     Returns:
-        resource_types |list of str|: A list resource types ('wood', 'ore', 'cloth', 'hide').
+        - stripped_type |List[str]| -- contains 1 resource type ('wood', 'ore', 'cloth', 'hide').
     '''
-    resource_types = []
+    stripped_type = []
 
-    for variable_name in variable_names:
+    for name in variable_names:
         # Remove any extra characters and split by underscores
-        parts = variable_name.strip().split('_')
+        parts = name.strip().split('_')
 
         if len(parts) >= 2:
             # Get the resource type and convert to lowercase
@@ -123,36 +150,43 @@ def strip_variable_name(variable_names):
 
             # Check if it's a valid resource type
             if resource_type in ('wood', 'ore', 'cloth', 'hide'):
-                resource_types.append(resource_type)
+                stripped_type.append(resource_type)
 
-    return resource_types
+    return stripped_type
 
-def create_csv_filename(resource_names):
+
+def generate_resource_csv_filename(stripped_type: List[str]) -> str:
     '''
-    Create a CSV filename based on resource name(s).
+    Generate CSV filename based on resource names.
 
     Args:
-        resource_names |list of str|: (e.g., ['wood'], ['ore'], ['cloth', 'hide']).
+        - stripped_type |List[str]| -- contains 1 resource type ('wood', 'ore', 'cloth', 'hide')
 
     Returns:
-        csv_filename |str|: The formatted CSV filename ('{}_refining.csv').
+        - filename |str|: Formatted CSV filename, e.g., 'wood_refining.csv'.
     '''
-    # Get the first element of the list (assuming it contains only one element)
-    resource_name = resource_names[0] if resource_names else 'unknown'
+    if stripped_type:
+        first_resource_name = stripped_type[0]
 
-    return f'{resource_name}_refining.csv'
-
-#####
-
+    filename = f'{first_resource_name}_refining.csv'
+    return filename
 
 
-def nr_of_daily_crafts(variable_name):
+def calculate_daily_crafts(variable_name: List[str]) -> int:
+    '''
+    Calculates and returns the number of daily crafts based on the item's tier and enchantment.
 
+    Args:
+        variable_name |List[str]| -- List generated by generate_variable_name() , item_id[0] req.
+
+    Returns:
+        - daily_crafts |int| -- The number of daily crafts for the specified tier.
+    '''
     if variable_name[0][-1] not in ['1', '2', '3', '4']:
         tier = f'{variable_name[0][1]}.0'
     else:
         tier = f'{variable_name[0][1]}.{variable_name[0][-1]}'
-        
+
     focus_cost_max_spec = {
         '4.0': 3, '4.1': 6, '4.2': 10, '4.3': 18, '4.4': 503,
         '5.0': 6, '5.1': 10, '5.2': 18, '5.3': 31, '5.4': 880,
@@ -168,39 +202,37 @@ def nr_of_daily_crafts(variable_name):
 
     return daily_crafts
 
-def show_max_price(item_ids, csv_filename):
 
-    #TODO: clean this shit up
+def find_maximum_sell_price(data_frame: pd.DataFrame) -> int:
+    '''
+    Finds and returns the maximum sell price from a DataFrame.
 
-    refined_item_id = item_ids[1]
-    tier_refined_item = int(refined_item_id[1])
-    tier_refined_item += 1
-    updated_tier_number = str(tier_refined_item)
-    updated_tier = refined_item_id[0] + updated_tier_number + refined_item_id[2:]
+    Args:
+        - data_frame |pd.DataFrame| -- The item data DataFrame from show_sell_price_df(
 
-    selected_columns = ['item_id', 'city', 'sell_price_max', 'sell_price_max_date']
-
-    data_frame = pd.read_csv(csv_filename)
-    data_frame['sell_price_max'] = data_frame['sell_price_max'].astype(float)
-    filtered_data_frame = data_frame[data_frame['item_id'] == updated_tier]
-
-    # Group by 'item_id' and sort within each group
-    result_data_frame = (filtered_data_frame[selected_columns]
-                         .groupby('item_id', as_index=True)
-                         .apply(lambda x: x.sort_values(by='sell_price_max', ascending=False)))
-
-    return result_data_frame
-
-def max_price_refined(data_frame):
-    #todo clean this shit
+    Returns:
+        - max_sell_price |int| -- The maximum sell price of the final product.
+    '''
     unique_item_id = data_frame["item_id"].unique()
 
     item_id = data_frame[data_frame['item_id'] == unique_item_id[0]]
-    item_id_price = item_id['sell_price_max'].min()
+    max_sell_price = int(item_id['sell_price_max'].max())
 
-    return item_id_price
+    return max_sell_price
 
-def calculate_daily_profit(daily_crafts, resource_value):
-    #todo this logic is shit
-    resource_returned = daily_crafts * resource_value * 0.539
-    return resource_returned
+
+
+def calculate_returned_resources_value(daily_crafts: int, resource_value: float) -> float:
+    '''
+    Calculates and returns the value of resources returned from daily crafts.
+
+    Args:
+        - daily_crafts |int| -- The number of daily crafts from calculate_daily_crafts()
+        - resource_value |float| -- The value of each resource from find_maximum_sell_price()
+
+    Returns:
+        - value_returned_resources |float| -- The value of resources returned.
+    '''
+    # Updated logic
+    value_returned_resources = daily_crafts * resource_value * 0.539
+    return value_returned_resources
